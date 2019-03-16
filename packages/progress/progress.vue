597c972f7989f98da798c553d08c3e06c3b0adfc
yushare <582718643@qq.com>
2019-03-16 14:31:51 +0800

    add

---
+++ packages/progress/progress.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div class="keep-progress">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'keep-progress'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../style/var";
+.keep-progress {
+  border: 1px solid #ccc;
+  outline: none;
+  padding: 5px 15px;
+  border-radius: 3px;
+  color: $blue;
+  background-color: #fff;
+}
+</style>
